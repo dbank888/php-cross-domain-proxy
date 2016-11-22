f7baff0ebad7a6531302aa5c7c82cefad865011f
Li Zongying <lizongying@msn.com>
2016-11-22 11:20:10 +0800

    update env

--- proxy.php
+++ proxy.php
@@ -23,13 +23,13 @@ define('CSAJAX_FILTER_DOMAIN', false);
 /**
  * Set debugging to true to receive additional messages - really helpful on development
  */
-define('CSAJAX_DEBUG', false);
+define('CSAJAX_DEBUG', true);
 
 /**
  * A set of valid cross domain requests
  */
 $valid_requests = array(
-    // 'example.com'
+     'http://112.124.11.61:3000/api'
 );
 
 /**
